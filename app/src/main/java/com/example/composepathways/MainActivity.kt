b4b5953269d0018ef4b97ebf7efb656f84f8f982
ruchajoshi <joshirucha0807@gmail.com>
2021-08-12 13:40:21 +0100

    Add List and Animate message while expanding

--- app/src/main/java/com/example/composepathways/MainActivity.kt
+++ app/src/main/java/com/example/composepathways/MainActivity.kt
@@ -6,12 +6,15 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
@@ -26,7 +29,8 @@ class MainActivity : AppCompatActivity() {
 
         setContent{
             ComposePathways{
-                MessageCard(Message("Android","Jetpeck Compose"))
+               // MessageCard(Message("Android","Jetpeck Compose"))
+                Conversation(SampleData.conversationSample)
             }
 
         }
@@ -35,7 +39,6 @@ class MainActivity : AppCompatActivity() {
 
 
 data class Message( val author: String,  val body: String)
-
 
 @Composable
 fun MessageCard(msg: Message) {
@@ -53,35 +56,61 @@ fun MessageCard(msg: Message) {
         )
 
         Spacer(modifier = Modifier.width(8.dp))
-        Column{
+
+
+        // We keep track if the message is expanded or not in this
+        // variable
+        var isExpanded by remember { mutableStateOf(false) }
+
+
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }){
             Text(text = msg.author,
                 color = MaterialTheme.colors.secondaryVariant,
-                style = MaterialTheme.typography.subtitle2
+                style = MaterialTheme.typography.subtitle1
             )
             Spacer(modifier = Modifier.height(4.dp))
 
             Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
                 Text(text = msg.body,
-                    color= MaterialTheme.colors.primaryVariant,
                     modifier = Modifier.padding(all = 4.dp),
-                    style = MaterialTheme.typography.body2
+                    // If the message is expanded, we display all its content
+                    // otherwise we only display the first line
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                    style = MaterialTheme.typography.subtitle2
                 )
             }
-
-
         }
     }
 
-
 }
 
-@Preview(name = "Light Mode")
-@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES,
-    showBackground = true,
-    name = "Dark Mode")
+//@Preview(name = "Light Mode")
+//@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES,
+//    showBackground = true,
+//    name = "Dark Mode")
+//@Composable
+//fun PreviewMessageCard() {
+//    ComposePathways {
+//        MessageCard(
+//            msg = Message("Android", "Jetpeck Compose")
+//        )
+//    }
+//}
+
+
 @Composable
-fun PreviewMessageCard() {
-    MessageCard(
-        msg= Message("Android","Jetpeck Compose")
-    )
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message ->
+            MessageCard(message)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewConversation() {
+    ComposePathways {
+        Conversation(SampleData.conversationSample)
+    }
 }
